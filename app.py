70d0aa8ae1de09b1ada4e73cfc65f4f0c1d04915
Shidosu <66335264+Shidosu@users.noreply.github.com>
2025-07-23 03:27:49 +0000

    1

--- app.py
+++ app.py
@@ -22,7 +22,15 @@ def index():
 @app.route("/main",methods=["GET","POST"])
 def main():
     q = request.form.get("q")
-    # db
+    # db - insert
+
+
+
+
+
+
+
+
     return(render_template("main.html"))
 
 
